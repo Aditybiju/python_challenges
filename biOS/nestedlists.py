69249c9d85912f004e3b8fe4065c928a14bd594e
Aditybiju <81827281+Aditybiju@users.noreply.github.com>
2021-05-04 22:04:39 +0530

    Update nestedlists.py

--- biOS/nestedlists.py
+++ biOS/nestedlists.py
@@ -3,16 +3,17 @@ mark=[]
 name=[]
 listofscores=[]
 
-
 for i in range (number_students):
 
     name=input()
     score=float(input())
     listofscores.append([name,score])
     mark.append(score)
-mark=sorted(set(mark))
+mark=sorted(mark)
+#print(listofscores)
+#print(mark)
 
-m=mark[0]
+m=mark[1]
 name=[]
 for j in listofscores:
     if m==j[1]:
